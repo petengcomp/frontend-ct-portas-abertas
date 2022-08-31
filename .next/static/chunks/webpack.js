25a1746c8be1af3b1edc83b519687329c5d0ea58
viniciuscole <viniamorim87@hotmail.com>
2022-08-31 19:29:44 -0300

    navbar redirecting pages correctly

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1c98a1d06355d80d"; }
+/******/ 		__webpack_require__.h = function() { return "34f28b7498b63801"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,7 @@
 import styles from '../styles/components/NavBar.module.css'
 import UfesLogo from '../assets/UfesLogo.svg'
 import Image from 'next/image'
+import Link from 'next/link'
 
 
 
@@ -16,10 +17,14 @@ export default function NavBar() {
         />
       </div>
       <ul>
-        <li id={styles.selected}>PÁGINA INICIAL</li>
+        <Link href="./">
+          <li id={styles.selected}>PÁGINA INICIAL</li>
+        </Link>
         <li>PROGRAMAÇÃO</li>
         <li>PALESTRANTES</li>
-        <li>CADASTRO</li>
+        <Link href="./signup">
+          <li>CADASTRO</li>
+        </Link>
       </ul>
     </div>
   )
