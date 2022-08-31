7abbb4c05bebd9c639a17f195beeb17e0f2a6905
KPMGE <jesuskevin254@gmail.com>
2022-08-30 22:35:20 -0300

    :recycle: rename RegisterStudentForm.module.css to RegisterForms.module.css

--- components/RegisterStudentForm.tsx
+++ components/RegisterStudentForm.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import styles from '../styles/components/RegisterStudentForm.module.css'
+import styles from '../styles/components/RegisterForms.module.css'
 import Button from './Button'
 
 export const RegisterStudentForm = () => {
