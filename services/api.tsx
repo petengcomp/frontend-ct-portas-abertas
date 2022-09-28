2ac08bedc361a8129646213e4a9c605c857dba2a
Matheus <mt.schreiber01@gmail.com>
2022-09-28 16:46:33 -0300

    🛂 changing api route

--- services/api.tsx
+++ services/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL:"http://localhost:3000/"
-  // baseURL:""
+  // baseURL:"http://localhost:3000/"
+  baseURL:"https://apictportasabertas.pet.inf.ufes.br"
 })           
