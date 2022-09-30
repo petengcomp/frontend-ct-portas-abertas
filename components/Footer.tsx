9ff6537703f664b0afac75476dfd21a20133a4a0
Matheus <mt.schreiber01@gmail.com>
2022-09-30 18:39:10 -0300

    :art: add ct logo to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,15 +3,19 @@ import styles from '../styles/components/Footer.module.css'
 import UfesLogo from '../assets/UfesLogo.svg'
 import IntrocompLogo from '../assets/IntrocompLogo.svg'
 import PETLogo from '../assets/PETLogo.svg'
+import CTLogo from '../assets/CT70ANOS.svg'
 
 export default function Footer() {
   return (
     <div className={styles.footerContainer}>
-      <div className={styles.upperContainer}>
+      <div className={styles.innerContainer}>
         <h3>Realização</h3>
-        <Image src={UfesLogo} alt="Ufes logo rodape" layout={'responsive'} />
+        <div>
+          <Image src={UfesLogo} alt="Ufes logo rodape" layout={'responsive'} />
+          <Image src={CTLogo} alt="Centro Tecnologico logo rodape" layout="responsive" />
+        </div>
       </div>
-      <div className={styles.lowerContainer}>
+      <div className={styles.innerContainer}>
         <h3>Apoio</h3>
         <div>
           <Image src={IntrocompLogo} alt="Introcomp logo rodape" layout="responsive" />
