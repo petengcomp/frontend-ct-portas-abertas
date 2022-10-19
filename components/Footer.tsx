e21f0b511c14d0c931c3e6361b4d4c2d6cae31f5
Matheus <mt.schreiber01@gmail.com>
2022-10-19 18:03:27 -0300

    :art: add new logos

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,23 +5,40 @@ import IntrocompLogo from '../assets/IntrocompLogo.svg'
 import PETLogo from '../assets/PETLogo.svg'
 import CTLogo from '../assets/CT70ANOS.svg'
 import NCDLogo from '../assets/NCDLogo.png' 
+import PROEXLogo from '../assets/PROEXLogo.png'
+import MCTILogo from '../assets/MCTILogo.png'
+import CNPQLogo from '../assets/CNPQLogo.png'
+import FNDCTLogo from '../assets/FNDCTLogo.png'
+
+import PETMECANICALogo from '../assets/PETMECANICALogo.png'
+import PETELETRICALogo from '../assets/PETELETRICALogo.png'
 
 export default function Footer() {
   return (
     <div className={styles.footerContainer}>
       <div className={styles.innerContainer}>
         <h3>Realização</h3>
-        <div>
-          <Image src={UfesLogo} alt="Ufes logo rodape" layout={'responsive'} />
-          <Image src={CTLogo} alt="Centro Tecnologico logo rodape" layout="responsive" />
+        <div className={styles.supportContainer}>
+          <div><Image src={UfesLogo} alt="Ufes logo rodape" layout="fill"  objectFit={'contain'} /></div>
+          <div><Image src={CTLogo} alt="Centro Tecnologico logo rodape" layout="fill"  objectFit={'contain'} /></div>
         </div>
       </div>
       <div className={styles.innerContainer}>
         <h3>Apoio</h3>
-        <div>
-          <Image src={IntrocompLogo} alt="Introcomp logo rodape" layout="responsive" />
-          <Image src={PETLogo} alt="PET logo rodape" layout="responsive" />
-          <Image src={NCDLogo} alt="NCD logo rodape" layout="responsive" />
+        <div className={styles.colaboratorsContainer}>
+          <div><Image src={IntrocompLogo} alt="Introcomp logo rodape" layout="fill"  objectFit={'contain'} /></div>
+          <div><Image src={PETLogo} alt="PET logo rodape" layout="fill"  objectFit={'contain'} /></div>
+          <div><Image src={NCDLogo} alt="NCD logo rodape" layout="fill"  objectFit={'contain'} /></div>
+          <div><Image src={PROEXLogo} alt="PROEX logo rodape" layout="fill"  objectFit={'contain'} /></div>
+          <div><Image src={MCTILogo} alt="MCTI logo rodape" layout="fill"  objectFit={'contain'} /></div>
+          
+        </div>
+
+        <div className={styles.colaboratorsContainer}>
+          <div><Image src={CNPQLogo} alt="CNPQ logo rodape" layout="fill"  objectFit={'contain'} /></div>
+          <div><Image src={FNDCTLogo} alt="FNDCT logo rodape" layout="fill"  objectFit={'contain'} /></div>
+          <div><Image src={PETMECANICALogo} alt="PET Mecanica logo rodape" layout="fill"  objectFit={'contain'} /></div>
+          <div><Image src={PETELETRICALogo} alt="PET Eletrica logo rodape" layout="fill"  objectFit={'contain'} /></div>          
         </div>
 
       </div>
