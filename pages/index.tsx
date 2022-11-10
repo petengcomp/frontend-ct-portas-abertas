79549f853074c66c7f3b9ff417e577f660e9d37d
Matheus <mt.schreiber01@gmail.com>
2022-11-10 00:11:08 -0300

    🔍 more tries to google search

--- pages/index.tsx
+++ pages/index.tsx
@@ -122,6 +122,14 @@ const Home: NextPage = () => {
   return (
     <main className={styles.container}>
 
+      <Head>
+        <title>CT de Portas Abertas - UFES - 2022</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta name="description" content="Espaço dedicado à inscrição no evento CT de Portas Abertas da UFES, venha conhecer!"/>
+        <meta content="CT de Portas Abertas - UFES - ES - 2022" property="og:title" />
+      </Head>
+
       <NavBar localPage={"index"} />
 
       <div className={styles.banner_container}>
